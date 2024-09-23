ba97827a321d514584419d305982b6a97c3913ba
mayank-dev07 <devmayank.hts@gmail.com>
2024-09-24 00:49:18 +0530

    feat: home page initialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,29 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+      <Image
+        src={"/bg.webp"}
+        alt="logo"
+        width={1200}
+        height={400}
+        className="w-full min-h-screen object-cover fixed top-0  z-0 blur-[4px] brightness-50"
+      />
       <main
         className={`flex min-h-screen items-center justify-center p-12 lg:py-24 `}
       >
         <div className="flex items-center justify-start flex-col lg:flex-row self-stretch">
           <div className="lg:w-1/2 flex flex-col items-start justify-center gap-16 z-20">
             <div className="flex flex-col ">
-              <h1 className="font-extrabold text-primary text-4xl text-theme">
+              <h1 className="font-extrabold text-primary text-4xl text-white">
                 SolFootball
               </h1>
-              <h1 className="font-extrabold text-xl text-theme">
+              <h1 className="font-extrabold text-xl text-white">
                 lets you challenge anyone in your favourite league
               </h1>
             </div>
 
             <div className="pl-0 sm:pl-10">
-              <p className="font-semibold text-theme">
+              <p className="font-semibold text-white">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Necessitatibus consectetur atque aspernatur quaerat temporibus
                 est nobis consequuntur autem, harum ratione nostrum, odio ullam
@@ -40,9 +47,9 @@ export default function Home() {
               </Link>
             </div>
           </div>
-          <div className="w-1/2">
+          {/* <div className="w-1/2">
             <Image src="/test2.png" alt="iu" width={500} height={500} />
-          </div>
+          </div> */}
         </div>
       </main>
     </>
